8b404e92261a748ae95078f543f1b07d05c28f38
Agus Dwi Sasongko <agusdwisasongko1@gmail.com>
2021-07-09 19:56:33 +0700

    add package - react-redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./assets/css/index.css";
 import App from "./routes/MainRoutes";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
