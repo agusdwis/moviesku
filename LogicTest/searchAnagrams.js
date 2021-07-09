a3518ee671391474a094e8f06a636f86d24e2cf5
Agus Dwi Sasongko <agusdwisasongko1@gmail.com>
2021-07-09 18:40:33 +0700

    Logic Test

---
+++ LogicTest/searchAnagrams.js
@@ -0,0 +1,30 @@
+function sortAnagrams(array) {
+  let found = {};
+  let res = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let substr = array[i].split("").sort().join("");
+    if (substr) {
+      found[substr] = found[substr] || [];
+      found[substr].push(array[i]);
+    }
+  }
+
+  for (let obj in found) {
+    res.push(found[obj]);
+  }
+
+  for (var i = 1; i < res.length; i += 1) {
+    if (res[i - 1].toString().length < res[i].toString().length) {
+      var tmp = res[i - 1];
+      res[i - 1] = res[i];
+      res[i] = tmp;
+    }
+  }
+
+  return res;
+}
+
+console.log(
+  sortAnagrams(["kita", "atik", "tika", "aku", "kia", "makan", "kua"])
+);
