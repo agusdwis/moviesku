440069bc56d5726861d72aa9c4d3278213095019
Agus Dwi Sasongko <agusdwisasongko1@gmail.com>
2021-07-09 19:29:37 +0700

    add packages - react router dom, prop-types axios redux redux-thunk

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HomePage = (props) => {
+  return (
+    <div>
+      <p className="text-red-700">MoviesKu Home</p>
+    </div>
+  );
+};
+
+HomePage.propTypes = {};
+
+export default HomePage;
---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+const MainRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+        </Switch>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default MainRoutes;
